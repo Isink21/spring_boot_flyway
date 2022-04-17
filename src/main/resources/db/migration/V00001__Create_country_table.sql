e55fac95e869046704cd85fa7d4847883796f8ec
Iliya Sinkevich <iliya.sinkevich@timvero.com>
2022-04-17 15:15:30 +0300

    first commit  Realized structure for test project

---
+++ src/main/resources/db/migration/V00001__Create_country_table.sql
@@ -0,0 +1,1 @@
+create table if not exists country (id int PRIMARY KEY, name varchar(30));
---
+++ src/test/java/ccom/github/javarushcommunity/springbootflywaydemo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ccom.github.javarushcommunity.springbootflywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
